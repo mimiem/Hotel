d040736018c59647bc83e7f836b645745d7ca7ef
Maria Vardalieva <mimiem87@gmail.com>
2017-05-08 00:50:08 +0300

    Added Administration Area with RoleController.cs

--- Source/Web/Hotel.Web/App_Start/RouteConfig.cs
+++ Source/Web/Hotel.Web/App_Start/RouteConfig.cs
@@ -39,7 +39,8 @@ namespace Hotel.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Hotel.Web.Controllers" }
             );
         }
     }
